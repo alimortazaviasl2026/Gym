06da11ce49c25023f3eabe67d95c1ac29d6c7628
ali <alimortazaviasl2026@gmail.com>
2026-08-16 11:03:39 +0330

    chang url text

--- src/components/features/HeroSection/HeroSection.jsx
+++ src/components/features/HeroSection/HeroSection.jsx
@@ -23,7 +23,7 @@ function HeroSection() {
                </Col >
                     <Col  xs={12} lg={6}>
                    <div >
-                        <img src="/Gym/Group-4.png"  className='w-100' />
+                        <img src="/Gym/Frame 314 4.png"  className='w-100' />
                    </div>
                </Col>
           </Row>
